d7ca679c0a8f4a6007c3031fdc8bc57e22a8072f
laihuy <minhbeoso1@gmai.com>
2024-12-07 22:44:09 +0700

    first commit

---
+++ codegit.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    echo "# duan2" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Lai-Huyy/duan2.git
+git push -u origin main
+    </p>
+    <p>
+    git remote add origin https://github.com/Lai-Huyy/duan2.git
+git branch -M main
+git push -u origin main
+    </p>
+</body>
+</html>
